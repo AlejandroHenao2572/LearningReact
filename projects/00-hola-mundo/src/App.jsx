415285a1c486752ecf63fb69b185ef0c10a637a8
Alejandro Henao <david.patacon-h@mail.escuelaing.edu.co>
2025-06-24 22:52:47 -0500

    Twitter button proyect

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt = "myAvatar" src="https://media1.tenor.com/m/3SHajRoXGRUAAAAd/explosion-cats.gif"  />
+                <div>
+                    <strong>Alejandro</strong>
+                    <span>@alejoCodes</span>
+                </div>
+            </header>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <aside>
+                <button>Follow</button>
+            </aside>
+        </article>
+    )
 }
-
-export default App
